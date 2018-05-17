1d492234e602b86a06e145ed8b192550d378b7a0
Julian Bertrix <julianbertrix@beweb-16.176Q>
2018-05-17 17:13:58 +0200

    mise page fini

--- contents/home.php
+++ contents/home.php
@@ -3,8 +3,26 @@
     body{ 
         background-image: url("http://www.lafontainevichy.fr/wp-content/uploads/2014/05/salle-restaurant-vichy.jpg");
     }
-    h1, h3, p{
+    h1, h3{
         text-align: center;
+    }
+    .col-sm-5{
+        border: solid;
+        border-width: thin;
+        width: 400px;
+        height: 200px;           
+        margin-top: 75px;
+        color: black;
+        background-color: silver;
+        margin-left: 100px;
+    }
+    
+    p{
+        margin-left: 100px;
+    }
+    
+    .news{
+        margin-left: 25px;
     }
 </style>
 
--- home.js
+++ home.js
@@ -5,15 +5,18 @@ var bootstrap = $("#content").append(
                 
 $(bootstrap).append($("<h1>").addClass("col-sm-12").text("Bienvenu sur RestauRaieO"));
 
-$(bootstrap).append($("<div>").addClass("col-sm-6")
+$(bootstrap).append($("<div>").addClass("col-sm-5")
             .append($("<h3>").text("Event"))
             .append($("<p>").text("01/07 soirée tapas et concert"))
             .append($("<p>").text("06/07 spécial Asie"))
             .append($("<p>").text("11/07 orgie")));
             
-$(bootstrap).append($("<div>").addClass("col-sm-6")
+$(bootstrap).append($("<div>").addClass("col-sm-2"));
+           
+$(bootstrap).append($("<div>").addClass("col-sm-5")
             .append($("<h3>").text("News"))
+            .append($("<div>").addClass("news")
             .append($("<p>").text("une news inutile"))
             .append($("<p>").text("une news qui peut servir"))
-            .append($("<p>").text("juste histoire de ...")));
+            .append($("<p>").text("juste histoire de ..."))));
 
