ba61daed3c9fa5855d962eb28b613b8e1767e51a
Julian Bertrix <julianbertrix@beweb-16.176Q>
2018-05-17 16:15:39 +0200

    ajout des news

--- contents/home.php
+++ contents/home.php
@@ -3,7 +3,7 @@
     body{ 
         background-image: url("http://www.lafontainevichy.fr/wp-content/uploads/2014/05/salle-restaurant-vichy.jpg");
     }
-    h1{
+    h1, h3, p{
         text-align: center;
     }
 </style>
