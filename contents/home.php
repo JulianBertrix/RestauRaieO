8ded86c4ef42eac41b3610d924535203819af891
Julian Bertrix <julianbertrix@beweb-16.176Q>
2018-05-17 16:08:46 +0200

    ajout d'un background + message de bienvenue + ajout d'infos sur les evenements + news en cours

--- contents/home.php
+++ contents/home.php
@@ -1,3 +1,10 @@
-<?php
-    echo "Bienvenue";
-?>
+<script src="../home.js"></script>
+<style>
+    body{ 
+        background-image: url("http://www.lafontainevichy.fr/wp-content/uploads/2014/05/salle-restaurant-vichy.jpg");
+    }
+    h1{
+        text-align: center;
+    }
+</style>
+
---
+++ home.js
@@ -0,0 +1,19 @@
+//$("#content").append($("<h1>").text("Bienvenu sur RestauRaieO"));
+var bootstrap = $("#content").append(
+           $("<div>").addClass("container-fluid").append(
+                $("<div>").addClass("container").attr("id", "contain")));
+                
+$(bootstrap).append($("<h1>").addClass("col-sm-12").text("Bienvenu sur RestauRaieO"));
+
+$(bootstrap).append($("<div>").addClass("col-sm-6")
+            .append($("<h3>").text("Event"))
+            .append($("<p>").text("01/07 soirée tapas et concert"))
+            .append($("<p>").text("06/07 spécial Asie"))
+            .append($("<p>").text("11/07 orgie")));
+            
+$(bootstrap).append($("<div>").addClass("col-sm-6")
+            .append($("<h3>").text("News"))
+            .append($("<p>").text("une news inutile"))
+            .append($("<p>").text("une news qui peut servir"))
+            .append($("<p>").text("juste histoire de ...")));
+
